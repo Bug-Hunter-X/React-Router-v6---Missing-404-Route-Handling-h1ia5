57e0b8094c285230448abed2668d90f107a78429
BugHunter-X <askzenai@gmail.com>
2025-02-11 03:34:06 +0100

    Add App.js file

---
+++ App.js
@@ -0,0 +1,17 @@
+```javascript
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        {/* Missing Route for path '*' to handle 404 error */}
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
+```
